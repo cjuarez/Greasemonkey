344bfe8d1c2e93ac29501656c6296eedcd8f8e27
Celso Ulises Juarez Ramirez <ulyman@gmail.com>
2013-01-01 13:32:42 -0600

    Added featured items
    Added featured items in Shops

--- etsyCleaner.js
+++ etsyCleaner.js
@@ -3,14 +3,18 @@
 // @namespace   https://github.com/cjuarez/Greasemonkey
 // @include      http://www.etsy.com/search/*
 // @author       Celso Juarez
-// @description Hides "Sponsored" results from Etsy search page
+// @description Hides "Sponsored", "Featured" and "Related" items in Etsy
 // @version     1.0
 // ==/UserScript==
 
 var pr = document.querySelector('#primary'),
     sp = document.querySelector('.s2'),
     re = document.querySelector('#related-searches'),
-    sh = document.querySelector('.shop-query-link');
+    sh = document.querySelector('.shop-query-link'),
+    ft = document.querySelectorAll('[id*=featured], [class*=featured]');
+if (!pr) {
+    pr = document.querySelector('.primary');
+}
 if (pr) {
     if (sp) {
         pr.removeChild(sp);
@@ -20,5 +24,10 @@ if (pr) {
     }
     if (sh) {
         pr.removeChild(sh);
+    }
+    if (ft.length > 0) {
+        for (var i = 0; i < ft.length; i++) {
+            pr.removeChild(ft.item(i));
+        }
     }
 }
